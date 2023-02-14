6bf2cb94cec0ba2e7dafc5208c9391673274561a
uldiniumGit <uldiniumGit@yandex.ru>
2023-02-14 16:11:17 +0500

    req

--- big_lesson/test_app/urls.py
+++ big_lesson/test_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'test_app'
 
 urlpatterns = [
-    path('', views.main_view),
-    path('pricing', views.show_prices),
-    path('about', views.about),
+    path('', views.main_view, name='index'),
+    path('pricing/', views.show_prices, name='pricing'),
+    path('about/', views.about, name='about'),
 ]

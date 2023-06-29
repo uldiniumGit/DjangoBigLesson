f2959f3e92d895961c5561e4b11459ac585dfaef
uldiniumGit <uldiniumGit@yandex.ru>
2023-06-29 17:44:29 +0500

    Tests

--- big_lesson/test_app/tests.py
+++ big_lesson/test_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import News
 
-# Create your tests here.
+
+# # Create your tests here.
+# class NewsTestCase(TestCase):
+#
+#     def setUp(self):
+#         self.news = News.objects.create(title='title', text='text')
+#
+#     def test_has_image(self):
+#         self.assertFalse(self.news.has_image())
+#
+#     def test_str(self):
+#         self.assertEqual(str(self.news), 'title')
